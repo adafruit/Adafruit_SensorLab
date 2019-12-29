9313aed81c9e9d7a261621da00c020b5844208a9
Lady Ada <limor@ladyada.net>
2019-12-28 19:10:50 -0500

    Init

---
+++ Adafruit_SensorLab.cpp
@@ -0,0 +1,17 @@
+/**************************************************************************/
+/*!
+    @file     Adafruit_SensorLab.cpp
+    @author   Limor Fried (Adafruit Industries)
+
+    Arduino library for scientific sensor readings/fusions/manipulations
+
+    Adafruit invests time and resources providing this open source code,
+    please support Adafruit and open-source hardware by purchasing
+    products from Adafruit!
+
+*/
+/**************************************************************************/
+
+#include <Adafruit_SensorLab.h>
+
+
---
+++ Adafruit_SensorLab.h
@@ -0,0 +1,21 @@
+/**************************************************************************/
+/*!
+    @file     Adafruit_SensorLab.h
+    @author   Limor Fried (Adafruit Industries)
+
+    Arduino library for scientific sensor readings/fusions/manipulations
+
+    Adafruit invests time and resources providing this open source code,
+    please support Adafruit and open-source hardware by purchasing
+    products from Adafruit!
+
+*/
+/**************************************************************************/
+
+#ifndef ADAFRUIT_SENSORLAB_H
+#define ADAFRUIT_SENSORLAB_H
+
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+
+#endif
