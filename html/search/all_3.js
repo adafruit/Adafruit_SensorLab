a5f1aaf56c86c9b7e2ab34eb05093540c0941a77
Doxygen CI <travis@travis-ci.org>
2020-01-26 07:18:49 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -12,5 +12,6 @@ var searchData=
   ['detectlis3mdl',['detectLIS3MDL',['../class_adafruit___sensor_lab.html#a2ca41b39feeb38991b5f94d624664ed7',1,'Adafruit_SensorLab']]],
   ['detectlsm6ds33',['detectLSM6DS33',['../class_adafruit___sensor_lab.html#aca26d0aaad7cb5bdba397b2f28618cf4',1,'Adafruit_SensorLab']]],
   ['detectlsm6dsox',['detectLSM6DSOX',['../class_adafruit___sensor_lab.html#ac7ac3372ac85424a206861f08bf4d6d6',1,'Adafruit_SensorLab']]],
-  ['detectmpu6050',['detectMPU6050',['../class_adafruit___sensor_lab.html#a6e4c77fc008e8d5be51b853bea88ac39',1,'Adafruit_SensorLab']]]
+  ['detectmpu6050',['detectMPU6050',['../class_adafruit___sensor_lab.html#a6e4c77fc008e8d5be51b853bea88ac39',1,'Adafruit_SensorLab']]],
+  ['detectmsa301',['detectMSA301',['../class_adafruit___sensor_lab.html#a8ce1d88c2daeea7a8cb89ca3746aa4bd',1,'Adafruit_SensorLab']]]
 ];
