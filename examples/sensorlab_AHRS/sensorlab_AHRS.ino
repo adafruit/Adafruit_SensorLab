d74a145dc73a8a23a2eaa208dd59f34483677081
Lady Ada <limor@ladyada.net>
2020-02-02 03:57:40 -0500

    add AHRS demo (mahony/madgwick/nxp)

---
+++ examples/sensorlab_AHRS/sensorlab_AHRS.ino
@@ -0,0 +1,123 @@
+// Full orientation sensing using NXP/Madgwick/Mahony and a range of 9-DoF
+// sensor sets.
+// You *must* perform a magnetic calibration before this code will work.
+//
+// To view this data, use the Arduino Serial Monitor to watch the
+// scrolling angles, or run the OrientationVisualiser example in Processing.
+// Based on  https://github.com/PaulStoffregen/NXPMotionSense with adjustments
+// to Adafruit Sensor interface and SensorLab
+
+#include <Adafruit_SensorLab.h>
+#include <Adafruit_Sensor_Calibration.h>
+#include <Adafruit_AHRS.h>
+
+// pick your filter! slower == better quality output
+Adafruit_NXPSensorFusion filter; // slowest
+//Adafruit_Madgwick filter;  // faster than NXP
+//Adafruit_Mahony filter;  // fastest
+
+#define FILTER_UPDATE_RATE_HZ 100
+uint32_t timestamp;
+
+Adafruit_SensorLab lab;
+Adafruit_Sensor_Calibration cal;
+Adafruit_Sensor *accelerometer, *gyroscope, *magnetometer;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) yield();
+
+  Serial.println(F("Sensor Lab - IMU AHRS!"));
+  lab.begin();
+  
+  if (!cal.begin()) {
+    Serial.println("Failed to initialize calibration helper");
+  } else if (! cal.loadCalibration()) {
+    Serial.println("No calibration loaded/found");
+  }
+
+  Serial.println("Looking for a magnetometer");
+  magnetometer = lab.getMagnetometer();
+  if (! magnetometer) {
+    Serial.println(F("Could not find a magnetometer!"));
+    while (1) yield();
+  }
+  
+  Serial.println("Looking for a gyroscope");
+  gyroscope = lab.getGyroscope();
+  if (! gyroscope) {
+    Serial.println(F("Could not find a gyroscope!"));
+    while (1) yield();
+  }
+  
+  Serial.println("Looking for a accelerometer");
+  accelerometer = lab.getAccelerometer();
+  if (! accelerometer) {
+    Serial.println(F("Could not find a accelerometer!"));
+    while (1) yield();
+  }
+  
+  accelerometer->printSensorDetails();
+  gyroscope->printSensorDetails();
+  magnetometer->printSensorDetails();
+
+  filter.begin(FILTER_UPDATE_RATE_HZ);
+  timestamp = millis();
+
+  Wire.setClock(400000); // 400KHz
+}
+
+
+void loop() {
+  float roll, pitch, heading;
+  float gx, gy, gz;
+
+  if ((millis() - timestamp) < (1000 / FILTER_UPDATE_RATE_HZ)) {
+    return;
+  }
+  timestamp = millis();
+  // Read the motion sensors
+  sensors_event_t accel, gyro, mag;
+  accelerometer->getEvent(&accel);
+  gyroscope->getEvent(&gyro);
+  magnetometer->getEvent(&mag);
+  //Serial.print("I2C took "); Serial.print(millis()-timestamp); Serial.println(" ms");
+
+  cal.calibrate(mag);
+  cal.calibrate(accel);
+  cal.calibrate(gyro);
+  // Gyroscope needs to be converted from Rad/s to Degree/s
+  // the rest are not unit-important
+  gx = gyro.gyro.x * SENSORS_RADS_TO_DPS;
+  gy = gyro.gyro.y * SENSORS_RADS_TO_DPS;
+  gz = gyro.gyro.z * SENSORS_RADS_TO_DPS;
+
+  // Update the SensorFusion filter
+  filter.update(gx, gy, gz, 
+                accel.acceleration.x, accel.acceleration.y, accel.acceleration.z, 
+                mag.magnetic.x, mag.magnetic.y, mag.magnetic.z);
+  //Serial.print("Update took "); Serial.print(millis()-timestamp); Serial.println(" ms");
+
+  Serial.print("Raw: ");
+  Serial.print(accel.acceleration.x, 4); Serial.print(", ");
+  Serial.print(accel.acceleration.y, 4); Serial.print(", ");
+  Serial.print(accel.acceleration.z, 4); Serial.print(", ");
+  Serial.print(gx, 4); Serial.print(", ");
+  Serial.print(gy, 4); Serial.print(", ");
+  Serial.print(gz, 4); Serial.print(", ");
+  Serial.print(mag.magnetic.x, 4); Serial.print(", ");
+  Serial.print(mag.magnetic.y, 4); Serial.print(", ");
+  Serial.print(mag.magnetic.z, 4); Serial.println("");
+
+  // print the heading, pitch and roll
+  roll = filter.getRoll();
+  pitch = filter.getPitch();
+  heading = filter.getYaw();
+  Serial.print("Orientation: ");
+  Serial.print(heading);
+  Serial.print(" ");
+  Serial.print(pitch);
+  Serial.print(" ");
+  Serial.println(roll);
+  Serial.print("Took "); Serial.print(millis()-timestamp); Serial.println(" ms");
+}
