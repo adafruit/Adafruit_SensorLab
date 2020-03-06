016dc70e239d0bd05c92fc84cec3dae79b809b9e
siddacious <nospam187+github@gmail.com>
2020-03-06 12:39:42 -0800

    doxy+clang

--- Adafruit_SensorLab.cpp
+++ Adafruit_SensorLab.cpp
@@ -283,27 +283,32 @@ bool Adafruit_SensorLab::detectICM20649(void) {
   delete _icm20649;
   return false;
 }
-
-bool Adafruit_SensorLab::detectHTS221(void){
+/**************************************************************************/
+/*!
+    @brief  Detect if we have a valid HTS221 sensor attached and sets
+    the default temperature and humidity sensors if so
+    @return True if found
+*/
+/**************************************************************************/
+bool Adafruit_SensorLab::detectHTS221(void) {
   bool addr5f = scanI2C(0x5F);
 
-  if (!addr5f){
+  if (!addr5f) {
     return false; // not even maybe!
   }
 
   _hts221 = new Adafruit_HTS221();
-  if (addr5f && _hts221->begin_I2C(0x5F)){
-    //we got one!
+  if (addr5f && _hts221->begin_I2C(0x5F)) {
+    // we got one!
     Serial.println(F("Found a HTS221 Humidity+Temperature sensor"));
     if (!humidity)
       humidity = _hts221->getHumiditySensor();
-    if(!temperature)
+    if (!temperature)
       temperature = _hts221->getTemperatureSensor();
     return true;
   }
   delete _hts221;
   return false;
-
 }
 /**************************************************************************/
 /*!
