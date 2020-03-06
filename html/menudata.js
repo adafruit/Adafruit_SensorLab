df37c8885c604066f65408b4d6efed9b689f4298
Doxygen CI <travis@travis-ci.org>
2020-03-06 22:25:53 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/menudata.js
+++ html/menudata.js
@@ -4,7 +4,13 @@ var menudata={children:[
 {text:"Class List",url:"annotated.html"},
 {text:"Class Index",url:"classes.html"},
 {text:"Class Members",url:"functions.html",children:[
-{text:"All",url:"functions.html"},
+{text:"All",url:"functions.html",children:[
+{text:"a",url:"functions.html#index_a"},
+{text:"b",url:"functions.html#index_b"},
+{text:"c",url:"functions.html#index_c"},
+{text:"d",url:"functions.html#index_d"},
+{text:"g",url:"functions.html#index_g"},
+{text:"m",url:"functions.html#index_m"}]},
 {text:"Functions",url:"functions_func.html"},
 {text:"Variables",url:"functions_vars.html"}]}]},
 {text:"Files",url:"files.html",children:[
